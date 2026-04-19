41d190a226fcf3f2a5a099119c4768ff641a3a3f
Narala Geetavarshini <23eg105j43@anurag.edu.in>
2026-04-19 22:15:35 +0530

    Update RootLayout.jsx

--- src/components/RootLayout.jsx
+++ src/components/RootLayout.jsx
@@ -1,5 +1,5 @@
 
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Outlet } from 'react-router';
 import Footer from './Footer';
 import Header from './Header';
@@ -7,35 +7,29 @@ import { useAuth } from '../AuthStore/useAuth';
 
 function RootLayout() {
   const isHydrated = useAuth((state) => state.isHydrated);
+  const checkAuth = useAuth((state) => state.checkAuth);
 
- 
+  
+  useEffect(() => {
+    if (isHydrated) {
+      checkAuth(); 
+    }
+  }, [isHydrated]);
+
   if (!isHydrated) {
     return (
-      <div className="min-h-screen flex flex-col justify-center items-center bg-gradient-to-br from-blue-600 via-indigo-600 to-purple-600">
-        
-        <div className="bg-white/10 backdrop-blur-lg p-8 rounded-2xl shadow-xl flex flex-col items-center">
-          
-          <div className="animate-spin h-12 w-12 border-4 border-white border-t-transparent rounded-full mb-4"></div>
-          
-          <h2 className="text-white font-semibold text-lg tracking-wide animate-pulse">
-            Loading your session...
-          </h2>
-        </div>
-
+      <div className="min-h-screen flex justify-center items-center bg-gradient-to-br from-blue-600 via-indigo-600 to-purple-600">
+        <p className="text-white text-lg">Loading your session...</p>
       </div>
     );
   }
 
   return (
-    <div className="flex flex-col min-h-screen bg-gradient-to-br from-gray-50 via-white to-blue-50">
-      
-      
+    <div className="flex flex-col min-h-screen">
       <Header />
-
       <main className="flex-grow">
         <Outlet />
       </main>
-
       <Footer />
     </div>
   );
